75be5b123c6c8ce834c396746bc4af7411ffa8f9
Farid-Mursa <faridfmur@code.edu.az>
2023-11-18 23:02:09 +0400

    About page add

---
+++ varkala_app/src/components/Contact/Contact.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <>
+      <section id="Hero">
+
+<div class="circle"></div>
+<img class="paralax" src="./assets/images/0cf24d64-4d11-4adc-ad08-e75a5d84f2db-removebg-preview (1).png" alt="Blog"/>
+
+<div class="container">
+  <p><span class="gold">Home</span> <span class="stick"> </span> <span>Contact</span></p>
+  <div class="content">
+    <h1>Contact</h1>
+  </div>
+</div>
+</section>
+    </>
+  );
+}
+
+export default Contact;

5f24d457cf92e4a2a673ba3e95c844cecf5ae2eb
Farid-Mursa <faridfmur@code.edu.az>
2023-11-19 23:37:05 +0400

    Contact page add

---
+++ varkala_app/src/pages/Contact.jsx
@@ -0,0 +1,47 @@
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <section id="SideBasket">
+      <div class="modal-dialog m-0 p-0">
+        <div class="modal-content parent">
+          <div class="modal-header">
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="products">
+              <div class="image">
+                <img src="./assets/images/product-square-ian-dooley-347968-unsplash.jpg" alt="">
+              </div>
+              <div class="name-price">
+                <p class="name">Skull Tee</p>
+                <p class="quantity">Quantity: <span class="baseCount">1</span></p>
+                <p class="name" style="font-family: monospace;">&75.00</p>
+              </div>
+              <div class="xBtn btn-close"></div>
+            </div>
+            <div class="products">
+              <div class="image">
+                <img src="./assets/images/product-square-ian-dooley-347968-unsplash.jpg" alt="">
+              </div>
+              <div class="name-price">
+                <p class="name">Skull Tee</p>
+                <p class="quantity">Quantity: <span class="baseCount">1</span></p>
+                <p class="name" style="font-family: monospace;">&75.00</p>
+              </div>
+              <div class="xBtn btn-close"></div>
+            </div>
+
+          </div>
+          <div class="modal-footer">
+            <div class="subTotal">
+              <p>Subtotal:</p>
+              <p class="total">$180.00</p>
+            </div>
+            <button class="view">View Cart</button>
+            <button>Checkout</button>
+          </div>
+          <div class="modal-footer">
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
